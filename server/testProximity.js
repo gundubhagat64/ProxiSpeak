8405ca276f0c207896ec962f5a117c42f053be1f
sapna-meena <meenasapna1919@gmail.com>
2026-09-02 10:54:50 +0530

    Add proximity radius tests

--- server/testProximity.js
+++ server/testProximity.js
@@ -7,7 +7,10 @@ const test = async () => {
   try {
     await connectDB();
 
-    console.log("Testing MongoDB proximity query...");
+    console.log("=== Proximity Testing Started ===");
+
+    // Test 1: Normal proximity query
+    console.log("\nTest 1: Normal query");
 
     const users = await findNearbyUsers(
       "test-user",
@@ -16,7 +19,7 @@ const test = async () => {
       100
     );
 
-    console.log("Nearby users:");
+    console.log("Nearby users:", users.length);
 
     users.forEach((user) => {
       console.log({
@@ -27,7 +30,37 @@ const test = async () => {
       });
     });
 
-    console.log(`Total nearby users: ${users.length}`);
+    // Test 2: Small radius
+    console.log("\nTest 2: Small radius");
+
+    const nearbySmallRadius = await findNearbyUsers(
+      "test-user",
+      400,
+      300,
+      30
+    );
+
+    console.log(
+      "Users within 30px:",
+      nearbySmallRadius.length
+    );
+
+    // Test 3: Large radius
+    console.log("\nTest 3: Large radius");
+
+    const nearbyLargeRadius = await findNearbyUsers(
+      "test-user",
+      400,
+      300,
+      200
+    );
+
+    console.log(
+      "Users within 200px:",
+      nearbyLargeRadius.length
+    );
+
+    console.log("\n=== Proximity Testing Completed ===");
 
     process.exit(0);
   } catch (error) {
